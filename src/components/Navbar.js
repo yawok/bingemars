71cdbb79b9d7ba3f5f3427ee29eba09894aef192
yaw.o.k <obengky72@gmail.com>
2024-04-29 17:30:18 +0200

    Add Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+
+export default function Navbar() {
+	return (
+		<nav>
+			<h1>Binge Mars</h1>
+			<section>
+				<div>
+					Curiosity
+				</div>
+				<div>
+					Perseverance
+				</div>
+				<div>
+					Opportunity
+				</div>
+			</section>
+		</nav>
+	)
+}
