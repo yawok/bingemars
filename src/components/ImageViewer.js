9d3a027294de843f1fe5a815285e99ee533e1855
yaw.o.k <obengky72@gmail.com>
2024-05-13 14:41:16 +0200

    Style image viewer component

--- src/components/ImageViewer.js
+++ src/components/ImageViewer.js
@@ -1,12 +1,12 @@
 export default function ImageViewer({ photo, currentIndex, photosLength, handleBack, handleNext }) {
 
 	return (
-		<div>
-			<button onClick={handleBack} disabled={currentIndex === 0}>Back</button>
+		<div className="image-viewer">
+			<button className="image-control-btn" onClick={handleBack} disabled={currentIndex === 0}>&lt; Previous</button>
 			<div>
-				<img src={photo.img_src} alt="photo" width='50%' height='50%' />
+				<img src={photo.img_src} alt="photo" />
 			</div>
-			<button onClick={handleNext} disabled={currentIndex === photosLength - 1}>Next</button>
+			<button className="image-control-btn" onClick={handleNext} disabled={currentIndex === photosLength - 1}>Next &gt;</button>
 		</div>
 	)
 }
