18b6a3444124e86a9ca29b32ed9c4b6310d98b40
yaw.o.k <obengky72@gmail.com>
2024-05-20 14:24:35 +0200

    Add loading error page.

---
+++ src/components/LoadingError.js
@@ -0,0 +1,5 @@
+const LoadingError = () => {
+	return <h1>Sorry, an error occured.</h1>
+}
+
+export default LoadingError
--- src/pages/LatestPhotos.js
+++ src/pages/LatestPhotos.js
@@ -1,14 +1,15 @@
 import { useEffect, useState } from "react";
 import ImageViewer from "../components/ImageViewer";
-import Loading from "../components/Loading";
 import RoverChoiceButton from "../components/RoverChoiceButton";
-import { DNA, ThreeCircles } from "react-loader-spinner";
+import { ThreeCircles } from "react-loader-spinner";
+import LoadingError from "../components/LoadingError";
 
 export default function LatestPhotos() {
 	const [photos, setPhotos] = useState([])
 	const [currentPhotoIndex, setCurrentPhotoIndex] = useState(0)
 	const [roverName, setRoverName] = useState('Curiosity')
 	const [isLoading, setIsLoading] = useState(true)
+	const [fetchError, setFetchError] = useState(false)
 	const rovers = ["Curiosity", "Perseverance"]
 
 	let url = `https://mars-photos.herokuapp.com/api/v1/rovers/${roverName}/latest_photos`
@@ -27,22 +28,23 @@ export default function LatestPhotos() {
 				setCurrentPhotoIndex(0)
 				setIsLoading(false)
 			}
-		).catch(error => console.log(error))
+		).catch(() => {
+			setIsLoading(false)
+			setFetchError(true)
+		})
 	}, [url])
 
 
 	return (
-		<div>
+		<div className="center-element">
 			{isLoading ? <ThreeCircles
 				visible={true}
 				height="100"
 				width="100"
 				color="#000"
 				ariaLabel="three-circles-loading"
-				wrapperStyle={{}}
-				wrapperClass="loading-spinner"
-			/> :
-
+				wrapperClass="center-element"
+			/> : fetchError ? <LoadingError /> :
 				<div className="latest-photos">
 					<div>
 						{
