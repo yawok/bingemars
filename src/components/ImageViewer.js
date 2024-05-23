90e2ac4e7ff12fac1dd77f44ab98c376e72a54f1
yaw.o.k <obengky72@gmail.com>
2024-05-23 23:32:04 +0200

    Add proper alt

--- src/components/ImageViewer.js
+++ src/components/ImageViewer.js
@@ -4,7 +4,7 @@ export default function ImageViewer({ photo, currentIndex, photosLength, handleB
 		<div className="image-viewer">
 			<button className="image-control-btn" onClick={handleBack} disabled={currentIndex === 0}>&lt; Previous</button>
 			<div>
-				<img src={photo.img_src} />
+				<img src={photo.img_src} alt={`id: ${photo.id}`} />
 			</div>
 			<button className="image-control-btn" onClick={handleNext} disabled={currentIndex === photosLength - 1}>Next &gt;</button>
 		</div>
