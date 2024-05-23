9b0941efc3043f3639723ac20c16bf3ec11ad978
yaw.o.k <obengky72@gmail.com>
2024-05-23 22:06:39 +0200

    Add tooltip to rover svgs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,19 @@
 import RoversPotrait from "../components/RoversPotraitSVG";
+import { Tooltip } from "react-tooltip";
 
+const rovers = ["Sojourner", "Spirit", "Opportunity", "Curiosity", "Perseverance", "Ingenuity"]
 export default function HomePage() {
 	return <div className="">
 		<section className="center-element header-text">
 			<h1> Watch all the pictures from the</h1>
 			<h1 className="red-planet">Red Planet</h1>
 		</section>
+		{
+			rovers.map((name, index) => (
+				<Tooltip anchorSelect={`.rover-svg${index}`} place="top">{name}</Tooltip>
+			)
+		)}
+		
 		<RoversPotrait className="rovers-potrait"/>
 	</div>
 }
